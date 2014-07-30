156dd9d6f8b78b9f3591c9d14ce8b3bfdf2ad5a4
Yang, Alex <alex.yang@cbsinteractive.com>
2014-07-30 02:18:11 -0700

    initial commit, need to add database

---
+++ app/controllers/Application.scala
@@ -0,0 +1,36 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+import play.api.data._
+import play.api.data.Forms._
+
+import models.Task
+
+object Application extends Controller {
+
+  def index = Action {
+    Redirect(routes.Application.tasks)
+  }
+
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = Action { implicit request =>
+    taskform.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
+
+  def deleteTask(id: Long) = TODO
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
+}
